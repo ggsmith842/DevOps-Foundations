e5de54b8fd6795501ca93cd9e36a570565f036e1
ggsmith842 <grant.oferrell.smith@gmail.com>
2022-09-14 19:53:47 -0600

    minimize and update code

--- code/main.go
+++ code/main.go
@@ -2,8 +2,18 @@ package main
 
 import(
 	"fmt"
+
 )
+
+func AddSum(a int, b int) int {
+	var myTotal int
+	myTotal = a + b
+
+	return myTotal
+}
+
 
 func main() {
 	fmt.Println("Hello World")
+	fmt.Println(AddSum(2, 3))
 }
